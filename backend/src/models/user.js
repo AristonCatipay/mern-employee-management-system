1b262ae9b26e26b2e8dae413e560ddd2aeb1f7a3
AristonCatipay <aristoncatipay123@gmail.com>
2025-05-12 23:32:18 +0800

    Update userSchema and move models folder to the src folder.

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -2,8 +2,20 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 3,
+    maxlength: 30,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
   role: { type: String, enum: ["Admin", "Employee"], required: true },
   profileImage: { type: String },
   createdAt: { type: Date, default: Date.now },
