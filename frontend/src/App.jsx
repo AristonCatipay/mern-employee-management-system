29e14c1f62ec2434cb7902e7b197ed1778383cb8
AristonCatipay <aristoncatipay123@gmail.com>
2025-05-14 23:17:06 +0800

    Create the NotFound component.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,26 @@
-import "./App.css";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Navigation from "./components/Navigation";
+import NotFound from "./pages/NotFound";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Container from "react-bootstrap/Container";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Welcome to the EMS</h1>
+      <Router>
+        <Navigation />
+        <Container>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="*" element={<NotFound></NotFound>}></Route>
+          </Routes>
+        </Container>
+      </Router>
     </>
   );
 }
